a222ebce75e92a2cb9c4a4ee17b722419459494c
vemoarrozypollo <veraomar50@gmail.com>
2023-03-14 17:20:33 -0500

    new card portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,6 +4,7 @@ import CardHref from '../components/CardHref';
 import rick from '../picturs/api-rickandmorthy.png';
 import ecommerce from '../picturs/e-commerce.png';
 import wheather from '../picturs/wheater-app.png';
+import Lumini from '../picturs/lumini.png';
 const Portfolio = () => {
   return (
     <Card className='justify-content-center' >
@@ -11,7 +12,8 @@ const Portfolio = () => {
           <CardGroup>
             <CardHref url={'https://polite-maamoul-8f220e.netlify.app/'} imag={ecommerce} titlecard={'E-Commerce'}/> 
             <CardHref url={'https://adorable-sundae-c494df.netlify.app/'} imag={rick} titlecard={'Rick And Morthy'}/>
-            <CardHref url={'https://graceful-centaur-4ccb88.netlify.app/'} imag={wheather} titlecard={'Wheather App'}/>                      
+            <CardHref url={'https://graceful-centaur-4ccb88.netlify.app/'} imag={wheather} titlecard={'Wheather App'}/>   
+            <CardHref url={'https://voluble-genie-013061.netlify.app/'}  imag={Lumini}  titlecard={'Lumini'}/>               
           </CardGroup>
         </Card>
   );
