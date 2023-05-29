410dd1b56a51516514140f9b54b37f42ea01df8b
vemoarrozypollo <veraomar50@gmail.com>
2023-05-29 18:09:04 -0500

    add site-pokeapi

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,6 +5,7 @@ import rick from '../picturs/api-rickandmorthy.png';
 import ecommerce from '../picturs/e-commerce.png';
 import wheather from '../picturs/wheater-app.png';
 import Lumini from '../picturs/lumini.png';
+import pokeapi from '../picturs/pokeapi.png';
 const Portfolio = () => {
   return (
     <Card className='justify-content-center' >
@@ -13,7 +14,8 @@ const Portfolio = () => {
             <CardHref url={'https://polite-maamoul-8f220e.netlify.app/'} imag={ecommerce} titlecard={'E-Commerce'}/> 
             <CardHref url={'https://adorable-sundae-c494df.netlify.app/'} imag={rick} titlecard={'Rick And Morthy'}/>
             <CardHref url={'https://graceful-centaur-4ccb88.netlify.app/'} imag={wheather} titlecard={'Wheather App'}/>   
-            <CardHref url={'https://voluble-genie-013061.netlify.app/'}  imag={Lumini}  titlecard={'Lumini'}/>               
+            <CardHref url={'https://voluble-genie-013061.netlify.app/'}  imag={Lumini}  titlecard={'Lumini'}/> 
+            <CardHref url={'https://apidexpoke.netlify.app/'} imag={pokeapi} titlecard={'Pokeapi'}/>
           </CardGroup>
         </Card>
   );
