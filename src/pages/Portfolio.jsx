b7a7cd8926071fde537e3944ac6d719efeb8f0c9
vemoarrozypollo <veraomar50@gmail.com>
2023-05-31 14:45:04 -0500

    order portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -12,10 +12,10 @@ const Portfolio = () => {
         <h1 className='blur-in-expand m-auto my-5'>Portfolio</h1>
           <CardGroup>
             <CardHref url={'https://polite-maamoul-8f220e.netlify.app/'} imag={ecommerce} titlecard={'E-Commerce'}/> 
-            <CardHref url={'https://adorable-sundae-c494df.netlify.app/'} imag={rick} titlecard={'Rick And Morthy'}/>
-            <CardHref url={'https://graceful-centaur-4ccb88.netlify.app/'} imag={wheather} titlecard={'Wheather App'}/>   
             <CardHref url={'https://voluble-genie-013061.netlify.app/'}  imag={Lumini}  titlecard={'Lumini'}/> 
             <CardHref url={'https://apidexpoke.netlify.app/'} imag={pokeapi} titlecard={'Pokeapi'}/>
+            <CardHref url={'https://adorable-sundae-c494df.netlify.app/'} imag={rick} titlecard={'Rick And Morthy'}/>
+            <CardHref url={'https://graceful-centaur-4ccb88.netlify.app/'} imag={wheather} titlecard={'Wheather App'}/>   
           </CardGroup>
         </Card>
   );
